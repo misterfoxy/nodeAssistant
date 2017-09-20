ad67e9564f4d59ff0bd8dc7c13bdf90712302495
Michael Fox <michaelscottfox1@gmail.com>
2017-09-19 18:43:42 -0700

    install packages, keys, and package.json

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'Q2mORFGJFYusaObOWvN2bBXi7',
+  consumer_secret: 'JWZywxlzMgt4nYNKwnMRTFdH4hZf770IW8a6hPDjBs9CUETauA',
+  access_token_key: '910253260803497984-EgRcru9IHQJYZuhoIXdJQO3xGm15SMY',
+  access_token_secret: 'STVvNyzOFs9bGzFLE6zpgNsMbHgWc8vaxV3VEiXUkj8Tf'
+};
+
+module.exports = twitterKeys;
